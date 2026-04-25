7972e2ee0634e3a091db05322d1c42d7f400976d
sakshi pandharpatte <sakship.1884@gmail.com>
2026-04-25 22:39:39 +0530

    frontend ready for deployment

--- sehatconnect_frontend/src/components/Navbar.jsx
+++ sehatconnect_frontend/src/components/Navbar.jsx
@@ -25,9 +25,9 @@ const Navbar = () => {
 
       {/* Navbar links */}
       <ul className={`nav-links ${menuOpen ? "open" : ""}`}>
-        <li><button onClick={() => scrollToSection("home")}>Home</button></li>
-<li><button onClick={() => scrollToSection("about")}>About</button></li>
-<li><button onClick={() => scrollToSection("contact")}>Contact</button></li>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#contact">Contact</a></li>
         <li>
           <Link to="/login">Log in</Link>
         </li>
