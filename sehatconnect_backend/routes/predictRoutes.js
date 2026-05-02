dc77d3e8c00e74e23787f6f2aa9eb0858506fc5d
sakshi pandharpatte <sakship.1884@gmail.com>
2026-05-02 23:34:54 +0530

    test route added 1

--- sehatconnect_backend/routes/predictRoutes.js
+++ sehatconnect_backend/routes/predictRoutes.js
@@ -3,6 +3,12 @@ import { predictRisk } from "../controllers/predictController.js";
 
 const router = express.Router();
 
+// POST route
 router.post("/", predictRisk);
+
+// TEST ROUTE (FIXED)
+router.get("/test-python", (req, res) => {
+  res.send("ROUTE IS WORKING");
+});
 
 export default router;
